96017aaf115cb0fc08736160c622e54dfefde255
Michael Yatco <mjyatco@gmail.com>
2017-03-02 21:24:49 -0500

    Converted Rails methods to AngularJS methods in API controller and views pages

--- app/assets/javascripts/meetings_ctrl.js
+++ app/assets/javascripts/meetings_ctrl.js
@@ -2,7 +2,12 @@
 (function() {
   "use strict";
 
-  angular.module("app").controller("meetingsCtrl", function($scope) {
+  angular.module("app").controller("meetingsCtrl", function($scope, $http) {
+    $scope.setup = function() {
+      $http.get("/api/v1/meeings.json").then(function(response) {
+        $scope.meetings = response.data;
+      });
+    };
     $scope.message = "Hello world!";
   });
 }());
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
   get '/meetings/:id/edit', to: 'meetings#edit'
   patch '/meetings/:id', to: 'meetings#update'
 
+  namespace :api do
+    namespace :v1 do
+      get '/meetings', to: 'meetings#index'
+      get '/tags', to: 'tags#index'
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
